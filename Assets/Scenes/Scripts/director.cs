fb7b6c962d313569b0b4912eb3db97cdbc0f5fd9
awuawulalala <1245330047@qq.com>
2018-05-18 22:15:59 +0800

    ARJumping的Demo版本，主要用来确定各种基本功能的课实现性与课操作性

---
+++ Assets/Scenes/Scripts/director.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class director : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject startBuilding;
+
+    public Text scoreText; //the sum of scores you win
+    public Text winText; //record winning info
+    public Slider Strength; //the state of holding the jumping button
+
+
+    // Use this for initialization
+    void Awake () {
+
+        Debug.Log("Starting Game!!!!!!!!!!!");
+
+        //inin do not start the player until we scanned the picture we want;
+        player.SetActive(false);
+
+        //do not run the script on Building until it is active
+        startBuilding.transform.GetComponent<activatePlayer>().enabled = false;
+
+        //set Strength
+        Strength.maxValue = playerController.HOLDINGLIMIT;
+        Strength.minValue = 0;
+        Strength.value = 0;
+
+        //init scores
+        playerController.scores = 0;
+        scoreText.text = "Score: " + playerController.scores;
+        scoreText.color = Color.blue;
+
+        //set winning text
+        winText.text = " ";
+        winText.fontSize = 20;
+        winText.color = Color.red;
+        winText.resizeTextMaxSize = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(startBuilding.activeSelf == true)
+        {
+            startBuilding.transform.GetComponent<activatePlayer>().enabled = true;
+        }
+
+        if (player.activeSelf == true)
+        {
+            Strength.value = playerController.holdingFrame;
+            scoreText.text = "Score: " + playerController.scores;
+            judgeGameState();
+        }
+
+    }
+
+    void judgeGameState()
+    {
+        if (playerController.gameState == 1) gameWinned();
+        else if (playerController.gameState == 2) gameFailed();
+    }
+
+    void gameWinned()
+    {
+        //send gameover info and restart the game
+        Debug.Log("Game Over");
+        winText.text = "Congratulations You Win! Your Score is " + playerController.scores;
+        StartCoroutine(waitAndRestart(2.0f));
+    }
+
+    void gameFailed()
+    {
+        winText.text = "YOU FAILED!!!";
+        StartCoroutine(waitAndRestart(2.0f));
+    }
+
+    IEnumerator waitAndRestart(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scenes/Scripts/playerController.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class playerController : MonoBehaviour
+{
+
+    public float speed; // player's speed
+
+    public static int HOLDINGLIMIT = 200; //the longest holding frame for jump
+
+    //store the building we players are now on
+    public GameObject building;
+    //store the destination building
+    public GameObject destination;
+
+    //create private reference to the rigidbody component on the player
+    private Rigidbody rb;
+    //determine the jumping direction
+    private Vector3 jumpDirection;
+    //judge whether player is on the ground
+    private bool isOnGround;
+
+    //store scores
+    public static int scores;
+    //judge the gameState ,0 for continue, 1 for victory,2 for lose
+    public static int gameState;
+    //store the holdingFrame
+    public static int holdingFrame;
+
+    void Start()
+    {
+
+        Debug.Log("activate player!!!!!!!!!!!!");
+
+        gameState = 0;
+
+        //give player a rigidbody
+        rb = GetComponent<Rigidbody>(); //assign regidbody to player
+
+        //init jump direction
+        jumpDirection = new Vector3(transform.forward.x, 1, transform.forward.z);
+
+        isOnGround = true; //init ison ground
+        holdingFrame = 0; //init holding frame
+
+        //init scores
+        scores = 0;
+
+        //cancel the conterforce
+        rb.freezeRotation = true;
+
+    }
+
+
+    void FixedUpdate()
+    {
+
+
+        //change direction of player
+        if (Input.GetKeyDown("w")) changeDirection();
+
+
+        //jump
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (holdingFrame < HOLDINGLIMIT)
+            {
+                Debug.Log("getting space!!!!!!!!!!!!");
+                holdingFrame++;
+            }
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (isOnGround == true)
+            {
+
+                Debug.Log("Adding Force!!!!!!!!!!!");
+                //move
+                rb.AddForce(jumpDirection * (speed + holdingFrame));
+                holdingFrame = 0;
+            }
+        }
+
+        if (transform.position.y <= 0) gameState = 2;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //jumping failed
+        if (other.collider.gameObject.tag == "Plane") gameState = 2;
+        //jumping successed
+        else if (other.collider.gameObject.tag == "Building" && other.collider.gameObject != building)
+        {
+            if (transform.position.y > other.collider.gameObject.transform.position.y)
+            {
+                building = other.collider.gameObject;
+                jumpSuccess();
+            }
+        }
+    }
+
+    void changeDirection()
+    {
+        //determine the roattion axis
+        Vector3 axis = new Vector3(0, 1, 0);
+        //determine the rotation angle
+        float angle = 45;
+        //rotatte
+        transform.Rotate(axis, angle);
+        jumpDirection = new Vector3(transform.forward.x, 1, transform.forward.z);
+    }
+
+    void jumpSuccess()
+    {
+        //set score
+        updateScore(distance());
+    }
+
+    float distance()
+    {
+        //get the distance from we player to the center of the building
+        return Mathf.Sqrt((transform.position.x - building.transform.position.x) * (transform.position.x - building.transform.position.x) + (transform.position.z - building.transform.position.z) * (transform.position.z - building.transform.position.z));
+    }
+
+    void updateScore(float dist)
+    {
+        //giving a score by the distance
+        if (dist > 1.4) scores += 1;
+        else if (dist > 1.0) scores += 2;
+        else if (dist > 0.6) scores += 4;
+        else if (dist > 0.2) scores += 6;
+        else scores += 10;
+
+        //judge if we reached the destination
+        if (building == destination) gameState = 1;
+    }
+
+}
