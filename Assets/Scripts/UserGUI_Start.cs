fd8ae6175dc2ef4f2a461f0fbf0df23b4f540906
awuawulalala <1245330047@qq.com>
2018-06-21 23:36:17 +0800

    Update UserGUI_Start.cs
    开始界面UI的功能更新

--- Assets/Scripts/UserGUI_Start.cs
+++ Assets/Scripts/UserGUI_Start.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,17 +16,36 @@ public class UserGUI_Start : MonoBehaviour
     //private IUserAction_Start action;
     private SSDirector director;
     private Button btn_start;
+    private Button btn_info;
+    private Button btn_quit;
 
     void Start()
     {
         director = SSDirector.getInstance();
         //action = director.currentSceneController as IUserAction_Start;
         btn_start = GameObject.Find("Canvas/NewGame/Start").GetComponent<Button>();
+        btn_info = GameObject.Find("Canvas/NewGame/Guide").GetComponent<Button>();
+
         btn_start.onClick.AddListener(scanScene);
+        btn_info.onClick.AddListener(readGuide);
     }
 
     public void scanScene()
     {
         director.play();
+    }
+
+    public void readGuide()
+    {
+        GameObject.Find("Canvas/NewGame/GuideContent").SetActive(true);
+
+        btn_quit = GameObject.Find("Canvas/NewGame/GuideContent/Quit").GetComponent<Button>();
+        btn_quit.onClick.AddListener(quitGuide);
+        
+    }
+
+    public void quitGuide()
+    {
+        GameObject.Find("Canvas/NewGame/GuideContent").SetActive(false);
     }
 }
