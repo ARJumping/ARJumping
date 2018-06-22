326ac7370c5cbc79090c2ac828c48ba3c292bf32
awuawulalala <1245330047@qq.com>
2018-06-22 22:18:31 +0800

    Update UserGUI_Start.cs
    update function of Start Menu

--- Assets/Scripts/UserGUI_Start.cs
+++ Assets/Scripts/UserGUI_Start.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,9 +17,13 @@ public class UserGUI_Start : MonoBehaviour
     private Button btn_start;
     private Button btn_info;
     private Button btn_quit;
+    private Button btn_pageUp;
+    private Button btn_pageDown;
+    private int pageState;
 
     void Start()
     {
+        pageState = 0;
         director = SSDirector.getInstance();
         //action = director.currentSceneController as IUserAction_Start;
         btn_start = GameObject.Find("Canvas/NewGame/Start").GetComponent<Button>();
@@ -38,14 +41,37 @@ public class UserGUI_Start : MonoBehaviour
     public void readGuide()
     {
         GameObject.Find("Canvas/NewGame/GuideContent").SetActive(true);
+        pageState = 1;
 
         btn_quit = GameObject.Find("Canvas/NewGame/GuideContent/Quit").GetComponent<Button>();
+        btn_pageUp = GameObject.Find("Canvas/NewGame/GuideContent/PageUp").GetComponent<Button>();
+        btn_pageDown = GameObject.Find("Canvas/NewGame/GuideContent/PageDown").GetComponent<Button>();
+
         btn_quit.onClick.AddListener(quitGuide);
+        btn_pageUp.onClick.AddListener(changePage);
+        btn_pageDown.onClick.AddListener(changePage);
         
     }
 
     public void quitGuide()
     {
+        pageState = 0;
         GameObject.Find("Canvas/NewGame/GuideContent").SetActive(false);
     }
+
+    public void changePage()
+    {
+        if(pageState == 1)
+        {
+            Image img = GameObject.Find("Canvas/NewGame/GuideContent").GetComponent<Image>() as Image;
+            img.sprite = Resources.Load("Images/secondPage", typeof(Sprite)) as Sprite;
+            pageState = 2;
+        }else if(pageState == 2)
+        {
+            Image img = GameObject.Find("Canvas/NewGame/GuideContent").GetComponent<Image>() as Image;
+            img.sprite = Resources.Load("Images/firstPage", typeof(Sprite)) as Sprite;
+            pageState = 1;
+        }
+    }
 }
+
